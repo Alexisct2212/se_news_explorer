ce6e1cae2019c5ac7d3f9e69fefa716d57a4c74e
Alexis Castillo <alexisct629@mac>
2025-04-22 22:39:39 -0400

    replace the about text with actual text

--- src/componets/About/About.jsx
+++ src/componets/About/About.jsx
@@ -6,8 +6,18 @@ function About() {
       <img className="about_me-avatar" src={avatar}/>
       <div className="around__circle">
       <h1 className="About_main-text">About the Author</h1>
-      <p className="about__subtext">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt aut culpa alias odit! Illo assumenda tempora rem fugiat ratione maiores? Beatae quisquam aut amet eveniet libero esse molestias nam suscipit?</p>
-      <p className="about__subtext_2">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Consectetur vitae delectus provident quod mollitia illum similique vel, molestiae temporibus, officiis deleniti aperiam assumenda iusto impedit, voluptatem eveniet minus incidunt id.</p>
+      <p className="about__subtext">My name is Alexis, I'm 22 years old. I've always been passionate about technology, 
+        and I decided to become a software engineer. I started studying at school, but they didn't teach me anything. 
+        I didn't have any interesting classes, so I decided to drop out, but I was always interested in programming. After a year, 
+        I decided to enroll in a program to learn and start a career. That's when I decided to enroll in Tripleten, and now I'm almost finished. 
+        I'm very happy to become a software engineer and start a career where I feel very confident in my abilities.
+
+      </p>
+      <p className="about__subtext_2">
+        My skills are Javascript,React,CSS,HTML,node.js,
+        mongoDB,SQL,Python,Git/bash and npm, 
+        for fun I like to play Videogames,go to the Gym and Drive
+      </p>
       </div>
     </div>
   );
